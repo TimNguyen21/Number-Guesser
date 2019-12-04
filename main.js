6b1417fa159f27391b16802f9fb0e59d392ac2ea
Jonathan Tschida <jonathan.tschida@outlook.com>
2019-12-03 21:03:07 -0700

    Added functionality to submit guess button to update latest guess info

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ var oneGuess = document.querySelector('#one-guess');
 var twoGuess = document.querySelector('#two-guess');
 var submitButton = document.querySelector('#submit-button');
 var clearButton = document.querySelector('#clear-button');
+var oneChallenger = document.querySelector('#one-challenger');
+var twoChallenger = document.querySelector('#two-challenger');
+var oneLatestGuess = document.querySelector('#one-latest-guess');
+var twoLatestGuess = document.querySelector('#two-latest-guess');
 
 submitButton.disabled = true;
 clearButton.disabled = true;
@@ -11,6 +15,7 @@ clearButton.disabled = true;
 document.addEventListener('keyup', enableSubmit);
 document.addEventListener('keyup', enableClear);
 clearButton.addEventListener('click', clearInputs);
+submitButton.addEventListener('click', updateLatestGuess);
 
 function enableSubmit() {
   var hasOneName = oneName.value !== "";
@@ -45,4 +50,13 @@ function clearInputs() {
   twoGuess.value = "";
   submitButton.disabled = true;
   clearButton.disabled = true;
+}
+
+function updateLatestGuess() {
+  oneChallenger.innerText = oneName.value;
+  twoChallenger.innerText = twoName.value;
+  oneLatestGuess.innerText = oneGuess.value;
+  twoLatestGuess.innerText = twoGuess.value;
+  oneGuess.value = "";
+  twoGuess.value = "";
 }
