778464e421ee661cd4d827fd29d966ce3d8652c2
Jonathan Tschida <jonathan.tschida@outlook.com>
2019-12-03 19:45:38 -0700

    Added functionality for enabling clear form button

--- main.js
+++ main.js
@@ -3,10 +3,13 @@ var twoName = document.querySelector('#two-name');
 var oneGuess = document.querySelector('#one-guess');
 var twoGuess = document.querySelector('#two-guess');
 var submitButton = document.querySelector('#submit-button');
+var clearButton = document.querySelector('#clear-button');
 
 submitButton.disabled = true;
+clearButton.disabled = true;
 
 document.addEventListener('keyup', enableSubmit);
+document.addEventListener('keyup', enableClear);
 
 function enableSubmit() {
   var hasOneName = oneName.value !== "";
@@ -18,5 +21,18 @@ function enableSubmit() {
     submitButton.disabled = false;
   } else {
     submitButton.disabled = true;
+  }
+}
+
+function enableClear() {
+  var hasOneName = oneName.value !== "";
+  var hasTwoName = twoName.value !== "";
+  var hasOneGuess = oneGuess.value !== "";
+  var hasTwoGuess = twoGuess.value !== "";
+  var isFilled = hasOneName || hasTwoName || hasOneGuess || hasTwoGuess;
+  if (isFilled) {
+    clearButton.disabled = false;
+  } else {
+    clearButton.disabled = true;
   }
 }
